d03c797b410b243cd0d12a462f5cabe2716209b2
Gabriel Henrique Rabelo Silva <gabrielhrabelosl@gmail.com>
2026-05-26 03:15:18 -0300

    added unique entity id value object

---
+++ src/core/entities/unique-entity-id.ts
@@ -0,0 +1,16 @@
+import { randomUUID } from 'node:crypto'
+
+export class UniqueEntityID {
+  public value: string
+
+  toString() {
+    return this.value
+  }
+
+  toValue() {
+    return this.value
+  }
+  constructor(value?: string) {
+    this.value = value ?? randomUUID()
+  }
+}
