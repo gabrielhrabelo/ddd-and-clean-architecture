eb16e9f3ec395dd874cf1f5f9e55ca186a17119e
Gabriel Henrique Rabelo Silva <gabrielhrabelosl@gmail.com>
2026-05-26 16:29:13 -0300

    added getters and setters

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,7 +1,8 @@
+import dayjs from 'dayjs'
 import { Entity } from '../../core/entities/entity.ts'
 import type { UniqueEntityID } from '../../core/entities/unique-entity-id.ts'
 import type { Optional } from '../../core/types/optional.ts'
-import type { Slug } from './value-objects/slug.ts'
+import { Slug } from './value-objects/slug.ts'
 
 interface QuestionProps {
   title: string
@@ -14,13 +15,67 @@ interface QuestionProps {
 }
 
 export class Question extends Entity<QuestionProps> {
+  public get content() {
+    return this.props.content
+  }
+
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get isNew(): boolean {
+    return dayjs().diff(this.createdAt, 'days') <= 3
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId
+  }
+  get slug() {
+    return this.props.slug
+  }
+
+  get title() {
+    return this.props.title
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get expect() {
+    return this.content.substring(0, 120).trimEnd().concat(' ...')
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+
+  set title(title: string) {
+    this.props.title = title
+    this.props.slug = Slug.createFromText(title)
+    this.touch()
+  }
+
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+    this.props.bestAnswerId = bestAnswerId
+  }
   static create(
-    props: Optional<QuestionProps, 'createdAt'>,
+    props: Optional<QuestionProps, 'createdAt' | 'slug'>,
     id?: UniqueEntityID,
   ) {
     const question = new Question(
       {
         ...props,
+        slug: props.slug ?? Slug.createFromText(props.title),
         createdAt: new Date(),
       },
       id,
